401a274b05b162fefcbce810c84594eb8db782a9
Jonathan Cheng <jcheng461@gmail.com>
2023-08-18 09:17:08 -0700

    refactor regex to utility function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
+// Utility Logic
+function numberRegex(userInput) {
+  let numberRegex = new RegExp(/^[0-9]+$/i);
+  let regexResult = numberRegex.test(userInput);
+  return regexResult;
+}
+
 // Business Logic
 function beepBoop(enteredNumber) {
   const saveNumbersUpToEnteredNumber = [];
-  // Set up Regex to see if input contains only numerical characters
-  let numberRegex = new RegExp(/^[0-9]+$/i);
-  let regexResult = numberRegex.test(enteredNumber);
-
   // Conditional statements to check if enteredNumber is a valid number
   if (enteredNumber < 0) {
     return null;
-  } else if (!regexResult) {
+  } else if (!numberRegex(enteredNumber)) {
     return NaN;
   }
   for (let i = 0; i <= enteredNumber; i++) {
