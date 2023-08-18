071e141ed79b5e39cba37faf18f41d98b290a675
Jonathan Cheng <jcheng461@gmail.com>
2023-08-18 09:13:12 -0700

    pass 3rd test for beepBoop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // Business Logic
 function beepBoop(enteredNumber) {
   const saveNumbersUpToEnteredNumber = [];
+  // Set up Regex to see if input contains only numerical characters
+  let numberRegex = new RegExp(/^[0-9]+$/i);
+  let regexResult = numberRegex.test(enteredNumber);
+
+  // Conditional statements to check if enteredNumber is a valid number
   if (enteredNumber < 0) {
     return null;
+  } else if (!regexResult) {
+    return NaN;
   }
   for (let i = 0; i <= enteredNumber; i++) {
     saveNumbersUpToEnteredNumber.push(i);
