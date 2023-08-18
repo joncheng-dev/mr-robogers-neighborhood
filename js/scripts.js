25f3214c6aebb982dca5e11431c42dead104ef86
Jonathan Cheng <jcheng461@gmail.com>
2023-08-18 08:39:07 -0700

    pass 1st test for beepBoop function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic
+function beepBoop(enteredNumber) {
+  const saveNumbersUpToEnteredNumber = [];
+  for (let i = 0; i <= enteredNumber; i++) {
+    saveNumbersUpToEnteredNumber.push(i);
+  }
+  console.log(saveNumbersUpToEnteredNumber);
+}
+
+// User Interface Logic
