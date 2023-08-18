f0cd053ac0b008a32116b0b96d63d08ee100c261
Jonathan Cheng <jcheng461@gmail.com>
2023-08-18 09:55:51 -0700

    past 4th test for beepBoop

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // Utility Logic
+// Regular Expression checks to see if input contains only number characters
 function numberRegex(userInput) {
   let numberRegex = new RegExp(/^[0-9]+$/i);
   let regexResult = numberRegex.test(userInput);
@@ -13,11 +14,23 @@ function beepBoop(enteredNumber) {
     return null;
   } else if (!numberRegex(enteredNumber)) {
     return NaN;
+  } else {
+    // Counts up from 0, pushing each number into an array
+    for (let i = 0; i <= enteredNumber; i++) {
+      saveNumbersUpToEnteredNumber.push(i);
+    }
+    console.log(saveNumbersUpToEnteredNumber);
+    // Maps through whole array. Saves elements into new array.
+    // If element is '1', then pushes "Beep" instead.
+    const substitutedResult = saveNumbersUpToEnteredNumber.map(function (element) {
+      if (element === 1) {
+        return "Beep!";
+      } else {
+        return element;
+      }
+    });
+    console.log(substitutedResult);
   }
-  for (let i = 0; i <= enteredNumber; i++) {
-    saveNumbersUpToEnteredNumber.push(i);
-  }
-  console.log(saveNumbersUpToEnteredNumber);
 }
 
 // User Interface Logic
